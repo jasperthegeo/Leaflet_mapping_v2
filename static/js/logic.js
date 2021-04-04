3396d94d194efbfbd430a376734aacb466252066
Jasper Parson <jasperthegeo@gmail.com>
2021-04-04 19:34:02 -0400

    updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -56,7 +56,7 @@ function createMap(data, plates) {
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: "mapbox/light-v10",
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
   });
 
@@ -66,24 +66,24 @@ function createMap(data, plates) {
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: "mapbox/satellite-v9",
+    id: "mapbox/satellite-v11",
     accessToken: API_KEY
   });
 
-  // outdoors
-  var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  // dark
+  var dark = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: "mapbox/outdoors-v11",
+    id: "mapbox/dark-v10",
     accessToken: API_KEY
   });
 
   var baseMaps = {
-    "Satellite": satellite,
-    "Grayscale": greyscale,
-    "Outdoors": outdoors
+    "Satellite": Satellite,
+    "Streets": greyscale,
+    "dark": Dark
   };
 
   var overlayMaps = {
