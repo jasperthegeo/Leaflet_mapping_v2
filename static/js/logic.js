9a0b877139a99a5ab3a192700143e9badce65897
Jasper Parson <jasperthegeo@gmail.com>
2021-04-04 20:15:31 -0400

    outdoors maps update

--- static/js/logic.js
+++ static/js/logic.js
@@ -80,10 +80,20 @@ function createMap(data, plates) {
     accessToken: API_KEY
   });
 
+  var outdoor = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v11",
+    accessToken: API_KEY
+  });
+
   var baseMaps = {
     "Satellite": satellite,
     "Streets": streets,
     "Dark": dark
+    "Outdoor":outdoor
   };
 
   var overlayMaps = {
